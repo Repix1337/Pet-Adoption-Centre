e8bc7281f9996c8b38bff4b8a3091c9a53d45551
Repix1337 <r3pig959@gmail.com>
2024-09-03 08:36:46 +0200

    final final version

--- src/pages/components/Main.js
+++ src/pages/components/Main.js
@@ -7,8 +7,8 @@ export function Main() {
     >
       <div className="absolute inset-0  bg-black bg-opacity-30" />
       <div className="relative z-10 flex flex-col justify-center items-center font-bold">
-        <h1 className="text-8xl text-white">Adopt a Pet</h1>
-        <button className="bg-gray-900 p-3 text-6xl  rounded-3xl bg-opacity-80 text-white w-2/3   mt-3">
+        <h1 className="text-8xl text-white mb-3">Adopt a Pet</h1>
+        <button className="bg-gray-900 p-3 text-6xl  rounded-3xl bg-opacity-80 text-white w-2/3  mt-3 hover:scale-110 hover:bg-sky-950 duration-500">
           <Link to="/search">Here</Link>
         </button>
       </div>
--- src/pages/components/PetItem.js
+++ src/pages/components/PetItem.js
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 export function PetItem({name, imageSrc}){
     return (
         <Link to='/search'>
-        <div className="appearAnimation bg-gray-100 text-center text-4xl text-black m-2 rounded-xl p-3 overflow-x-hidden">
+        <div className="appearAnimation bg-gray-800 text-center text-4xl text-white m-2 rounded-xl p-3 overflow-x-hidden hover:scale-105 hover:bg-sky-950 duration-500">
             <img src={imageSrc} alt={name} className="w-64 h-52 object-cover"/>
             {name}
         </div>
